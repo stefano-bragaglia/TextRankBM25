3081fa455ae5e9333f7e28635cd3f62cef505c2b
Stefano <stefano.bragaglia@gmail.com>
2016-03-22 14:21:34 +0000

    first commit

---
+++ src/main/java/example/Application.java
@@ -0,0 +1,15 @@
+package example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO Add some meaningful class description...
+ */
+public class Application {
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		logger.info("Done.");
+	}
+}
